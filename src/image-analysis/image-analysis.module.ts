7f61fadcb6e4677f4be346f180b92881391dcd7e
alejoboga20 <alejoboga19@gmail.com>
2025-05-31 23:45:56 -0300

    feat(image) implement image recognotion

--- src/image-analysis/image-analysis.module.ts
+++ src/image-analysis/image-analysis.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ImageAnalysisService } from './image-analysis.service';
 import { ImageAnalysisController } from './image-analysis.controller';
+import { OpenAiModule } from 'src/openai/openai.module';
 
 @Module({
   controllers: [ImageAnalysisController],
   providers: [ImageAnalysisService],
+  imports: [OpenAiModule],
 })
 export class ImageAnalysisModule {}
--- src/image-analysis/image-analysis.service.ts
+++ src/image-analysis/image-analysis.service.ts
@@ -1,16 +1,40 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { OpenAiService } from '../openai/openai.service';
 
 @Injectable()
 export class ImageAnalysisService {
   private readonly logger = new Logger(ImageAnalysisService.name);
 
-  processImage(imageFile: Express.Multer.File) {
+  constructor(private readonly openAiService: OpenAiService) {
+    this.logger.log('ImageAnalysisService initialized');
+  }
+
+  async processImage(imageFile: Express.Multer.File) {
     this.logger.log('Processing file');
     const base64Image = imageFile.buffer.toString('base64');
 
-    return {
-      message: 'Image received',
-      base64Image,
-    };
+    try {
+      const response = await this.openAiService.analyseImage(base64Image);
+
+      if (!response) {
+        this.logger.error('No response from OpenAI service');
+        throw new InternalServerErrorException(
+          'No response from OpenAI service',
+        );
+      }
+
+      if (response.detected === false) {
+        return { detected: false, message: 'No meal detected in the image' };
+      }
+      /* TODO: Calculate Macros based on formulas, save calcualted calories and compare them with the predicted by GPT */
+      return response;
+    } catch (error) {
+      this.logger.error('Error processing image', error);
+      throw new InternalServerErrorException('Failed to analyze image');
+    }
   }
 }
