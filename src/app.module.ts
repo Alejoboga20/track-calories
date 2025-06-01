33af19be49475ecab00fda45d31f7fe78acf9b48
alejoboga20 <alejoboga19@gmail.com>
2025-05-31 22:51:32 -0300

    chore(env) config env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ImageAnalysisModule } from './image-analysis/image-analysis.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ImageAnalysisModule],
+  imports: [ConfigModule.forRoot(), ImageAnalysisModule],
   controllers: [],
   providers: [],
 })
---
+++ src/config/envs.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  OPENAI_API_KEY: string;
+}
+
+const envVarsSchema = joi
+  .object({
+    OPENAI_API_KEY: joi.string().required(),
+  })
+  .unknown(true);
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const { error, value } = envVarsSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars = value as EnvVars;
+
+export const envConfigVars = {
+  openAiApiKey: envVars.OPENAI_API_KEY,
+};
